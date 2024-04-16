58f38d87f3a3e38e5c037a6b323366082d899780
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2024-04-16 19:28:03 +0000

    commands

--- build_files.sh
+++ build_files.sh
@@ -1,2 +1,6 @@
+python3.9 -m venv blogapienv
+source blogapienv/bin/activate
+pip install -r requirements.txt 
+python3.9 manage.py collectstatic
 pip install -r requirements.txt 
 python3.9 manage.py collectstatic
