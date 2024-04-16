e304e50437dc1b48ba3f5725c16f579ce36c243d
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2024-04-16 22:12:58 +0000

    requirements file

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,2 @@
-python3.9 -m venv blogapienv
-source blogapienv/bin/activate
-pip install -r requirements.txt 
-python3.9 manage.py collectstatic
 pip install -r requirements.txt 
 python3.9 manage.py collectstatic
