a56ed15cee52c812aafa18ab5d3ec153504c96d7
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2024-04-17 13:57:48 +0000

    vercel advance config file

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,16 @@
-pip install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic
+{
+  "builds": [
+    {
+      "src": "build_files.sh",
+      "use": "@vercel/build-script",
+      "config": {
+        "commands": [
+          "python -m venv venv",  
+          "source venv/bin/activate",  
+          "pip install -r requirements.txt"  
+        ]
+      }
+    }
+  ],
+  "functions": { "prebuild": "build_files.sh" }  
+}
