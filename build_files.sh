9212bcbe7d0d110ca00c543b2837446b081adcae
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2024-04-20 23:00:55 +0000

    vercel conf

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,6 @@
+sudo apt-get install --reinstall python-pkg-resources
+python3 -m pip install --upgrade setuptools
+python3 -m pip install --upgrade distribute 
 python3 -m pip install -r requirements.txt
 python3 manage.py migrate
 python3 manage.py collectstatic
