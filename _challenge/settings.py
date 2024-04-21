023b85db9e6fe70d527f3f30f41784df325687cf
Williamdreams89 <wdanquah@stu.ucc.edu.gh>
2024-04-21 07:06:28 +0000

    Authentication headers for jwt token

--- _challenge/settings.py
+++ _challenge/settings.py
@@ -128,9 +128,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = "usersapp.User"
 REST_FRAMEWORK = {
    'DEFAULT_AUTHENTICATION_CLASSES': (
-       'rest_framework.authentication.TokenAuthentication',
+       'rest_framework.authentication.JWTAuthentication',
    ),
    'DEFAULT_PERMISSION_CLASSES': (
-        'rest_framework.permissions.AllowAny'
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
    ),
 }
